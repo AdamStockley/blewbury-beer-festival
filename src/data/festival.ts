9dbff8c688ba08b8cd14d023e704cf7a51068dfa
AdamStockley <44431649+AdamStockley@users.noreply.github.com>
2026-08-08 10:46:12 +0100

    Create initial Blewbury Beer Festival 2026 site framework

---
+++ src/data/festival.ts
@@ -0,0 +1,42 @@
+export const festival = {
+  name: "Blewbury Beer Festival",
+  year: 2026,
+  date: "Saturday 3 October 2026",
+  shortDate: "3 October 2026",
+  location: "Blewbury, Oxfordshire",
+  strapline: "Supporting good causes through the consumption of beer…",
+  intro:
+    "A relaxed village beer festival bringing together great drinks, food and live entertainment while raising money for good causes.",
+};
+
+export const highlights = [
+  {
+    icon: "🍺",
+    title: "Beer & Cider",
+    text: "We're putting together the 2026 drinks selection now. Breweries, beers and ciders will be added as they're confirmed.",
+    href: "/drinks",
+    linkText: "Explore the drinks",
+  },
+  {
+    icon: "🎵",
+    title: "Live Entertainment",
+    text: "The 2026 entertainment programme is taking shape. We'll announce performers and timings here as soon as they're confirmed.",
+    href: "/entertainment",
+    linkText: "See entertainment",
+  },
+  {
+    icon: "🍴",
+    title: "Food",
+    text: "Food details are still being finalised. Expect something good to accompany the beer.",
+    href: "/food",
+    linkText: "Food information",
+  },
+];
+
+export const news = [
+  {
+    date: "8 August 2026",
+    title: "Blewbury Beer Festival returns this October",
+    text: "Planning is underway for the next Blewbury Beer Festival on Saturday 3 October 2026.",
+  },
+];
